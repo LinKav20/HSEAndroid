07bccd31356e5d3e70abcb613e5640fa1400f11a
LinKa <maltseva-as@mail.ru>
2021-12-09 20:02:42 +0300

    task7 done

---
+++ task7/app/src/main/java/com/example/task7/MapsActivity.kt
@@ -0,0 +1,39 @@
+package com.example.task7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.task7.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val moscow = LatLng(55.751244, 37.618423)
+        mMap.addMarker(MarkerOptions().position(moscow).title("Hi there!"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(moscow))
+    }
+}
