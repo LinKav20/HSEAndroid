32cda0e0b95efd39c7a633c72d08a7b901617634
LinKa <maltseva-as@mail.ru>
2021-12-11 15:58:40 +0300

    task10 done

---
+++ task10/app/src/main/java/com/example/firsttask/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.firsttask
+
+import android.content.DialogInterface
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var editText: EditText;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        editText = findViewById(R.id.editText)
+
+        registerForContextMenu(editText)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.makeBold) {
+            if (item.isChecked) {
+                editText.typeface = Typeface.DEFAULT;
+                item.isChecked = false;
+            } else {
+                editText.typeface = Typeface.DEFAULT_BOLD;
+                item.isChecked = true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        menuInflater.inflate(R.menu.context_menu, menu)
+        super.onCreateContextMenu(menu, v, menuInfo)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.erase -> showDialog()
+        }
+        return super.onContextItemSelected(item)
+    }
+
+    fun showDialog(){
+        val dialog = AlertDialog.Builder(this)
+        dialog.setTitle("Are you sure?")
+        dialog.setMessage("Do you want to clear the field?")
+        dialog.setPositiveButton("Yes", { dialogInterface: DialogInterface, i: Int ->
+            editText.setText("")
+        })
+
+        dialog.setNegativeButton("No", { dialogInterface: DialogInterface, i: Int -> })
+        dialog.show()
+    }
+}
