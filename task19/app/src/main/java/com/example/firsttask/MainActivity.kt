847fe0af6aa9610a9567984b6b413747caaa0b03
LinKa <maltseva-as@mail.ru>
2022-03-10 20:22:20 +0300

    task19 done

--- task19/app/src/main/java/com/example/firsttask/MainActivity.kt
+++ task19/app/src/main/java/com/example/firsttask/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.firsttask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartView
+import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val aaChartView : AAChartView = findViewById(R.id.aa_chart_view)
+        val aaChartModel : AAChartModel = AAChartModel()
+            .chartType(AAChartType.Area)
+            .title("Some data")
+            .subtitle("subtitle")
+            .backgroundColor("#ebdecf")
+            .dataLabelsEnabled(true)
+            .series(arrayOf(
+                AASeriesElement()
+                    .name("First")
+                    .data(arrayOf(7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6)),
+                AASeriesElement()
+                    .name("Second")
+                    .data(arrayOf(3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 14.8))
+            )
+            )
+        aaChartView.aa_drawChartWithChartModel(aaChartModel)
     }
 }
